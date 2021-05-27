85f62023ca5340ae8f5d557e3b5d019e54b1015d
adminhelper <201401339hs@gmail.com>
2021-05-27 15:33:59 +0900

    (feat) : setTest

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -1,5 +1,45 @@
 package study;
 
-public class SetTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.in;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp(){
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    // Test Case 구현
+    @Test
+    @DisplayName("요구사항 1")
+    void size(){
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+    @Test
+    @DisplayName("요구사항 2")
+    void contains(){
+        assertThat(numbers.contains(1)).isTrue();
+        assertThat(numbers.contains(2)).isTrue();
+        assertThat(numbers.contains(3)).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3})
+    void isBlank_ShouldReturnTrueForNullOrBlankStrings(Integer input) {
+        assertThat(numbers.contains(input)).isTrue();
+    }
 }
